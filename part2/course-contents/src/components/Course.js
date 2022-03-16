c3c0766da15b03816201dddf07dc66661d004ba4
harunmohamed <harunmohamed901@gmail.com>
2022-03-16 15:08:19 +0200

    part2.1

---
+++ part2/course-contents/src/components/Course.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Header = props => (
+    <h1>{props.course}</h1>
+)
+
+const Total = ({parts}) => {
+    const total = parts.reduce((acc, val) => acc + val.exercises, 0)
+    return <h3>total of {total} exercises</h3>
+}
+
+const Part = props => (
+    <p> {props.part.name} {props.part.exercises} </p>
+)
+
+const Content = props => {
+    return props.parts.map((part) => <Part key={part.id} part={part} />)
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course;
