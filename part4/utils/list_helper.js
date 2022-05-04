b0daf53ca1e1895c2f0d3af6e2aec992948c756b
harunmohamed <harunmohamed901@gmail.com>
2022-05-05 00:19:52 +0300

    part4.4 - 4.7 testing with jest

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,62 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
-    return 1;
+  return 1;
+}
+const totalLikes = (blogs) =>{
+
+  if (blogs.length === 0 ) return 0
+  
+  const result = blogs.reduce((a,b) => {
+    return { likes: a.likes + b.likes }
+  })
+    
+  return result.likes
+
 }
 
-module.exports = { dummy }
+const favoriteBlog = (blogs) => {
+   if(blogs.length === 0 ) return {}
+   return blogs.reduce((highest, blog) => 
+   highest.likes > blog.likes ? { 
+    title : highest.title, 
+    author: highest.author,
+    likes: highest.likes
+   } : {
+    title : blog.title, 
+    author: blog.author,
+    likes: blog.likes
+    })
+}
+
+const mostBlogs = (blogs) => {
+  const sorted = _.countBy(blogs, 'author')
+  const listed = []
+  _.forOwn(sorted, (value, key) => {
+    listed.push({
+      author: key,
+      blogs: value
+    })
+  })
+  return _.maxBy(listed, 'blogs')
+}
+
+const mostLikes = (blogs) => {
+  const sorted = _.groupBy(blogs, 'author')
+  const myList = []
+  _.forOwn(sorted, (value, key) => {
+    myList.push({
+      author: key,
+      likes: value.reduce((acc, blog) => acc + blog.likes, 0)
+    })
+  })
+  return _.maxBy(myList, 'likes')
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
